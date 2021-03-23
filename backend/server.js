ebbf2c2d9eacf795e411974b064923ca542cb701
CD-Mackay <connor.mackay@gmail.com>
2021-03-23 13:02:36 -0400

    add pg pool to server

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,24 @@ const app = express();
 const PORT = process.env.port || 8080;
 
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}!`));
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  user: 'connormackay'
+});
+
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('error acquiring client', err.stack);
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release()
+    if(err) {
+      return console.error('error executing query', err.stack);
+    }
+    console.log(result.rows);
+  });
+});
