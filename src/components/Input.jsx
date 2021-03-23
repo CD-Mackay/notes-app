2e4607a8f3dbcd3fb840e57be3d89ff833f9a054
CD-Mackay <connor.mackay@gmail.com>
2021-03-23 12:39:45 -0400

    add button to form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Button from './Button';
 
 export default function Input() {
   return (
     <form>
       <textarea>
-        
+
       </textarea>
+      <Button message="save" />
+      <Button message="delete" />
     </form>
   )
 }
