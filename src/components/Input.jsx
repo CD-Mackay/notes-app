db42a4653dbc1c2f6ff0759ddcc1e46ccec0a9b0
CD-Mackay <connor.mackay@gmail.com>
2021-03-24 11:24:44 -0400

    add state to form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 export default function Input(props) {
-
+  const [state, setState] = useState({
+    title: "",
+    text: ""
+  });
 
   return (
     <form onSubmit={event => event.preventDefault}>
       <label for="title">Title</label>
-      <input type="text" name="title"></input>
-      <textarea name="text">
+      <input type="text" name="title"  onChange={event => setState(prev => ({...prev, title: event.target.value, text: ""}))}></input>
+      <textarea name="text" onChange={event => setState(prev => ({...prev, text: event.target.value}))}>
 
       </textarea>
       <Button type="submit" message="save" onClick={props.onSave} />
