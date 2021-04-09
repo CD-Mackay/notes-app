f5e86da00e22ab9d3abea8f623a48660c121229a
CD-Mackay <connor.mackay@gmail.com>
2021-04-09 13:20:21 -0400

    style header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import './styles.scss';
 export default function Header () {
   return (
-    <h1>I am header</h1>
+    <div className="header">
+      <h1>Notes by Connor Mackay Development</h1>
+      <h3>A React App</h3>
+    </div>
   )
 }
