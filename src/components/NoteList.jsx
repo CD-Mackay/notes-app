fe1c4ea9cda47e4e994c4ad70b04c2227a0fb349
CD-Mackay <connor.mackay@gmail.com>
2021-04-12 13:49:30 -0400

    debug rerender issue

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -4,13 +4,13 @@ import './styles.scss';
 
 export default function NoteList(props) {
 
-  const parsedNotes = props.savedNotes.map(note => {
-    if (note.note) {
-      const focus = note.note._immutable.selection.focusKey;
-      console.log(note.note._immutable.currentContent.blockMap[focus].text);
-      const content = note.note._immutable.currentContent.blockMap[focus].text;
-    return <NoteListItem content={note.note._immutable.currentContent.blockMap} focus={note.note._immutable.selection.focusKey} noteId={note.id} key={note.id} title={note.note.title} delete={props.onDelete} text={content} />
-    }
+    const parsedNotes = props.savedNotes.map(note => {
+      if (note.note) {
+        const focus = note.note._immutable.selection.focusKey;
+        const content = note.note._immutable.currentContent.blockMap[focus].text;
+      return <NoteListItem content={note.note._immutable.currentContent.blockMap} focus={note.note._immutable.selection.focusKey} noteId={note.id} key={note.id} title={note.note.title} delete={props.onDelete} text={content} />
+      }
+
   })
   return (
     <div className="notes">
