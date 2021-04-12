0a4a812529758746b12244a23d07d201841f6c45
CD-Mackay <connor.mackay@gmail.com>
2021-04-12 12:45:17 -0400

    refactor output component

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -6,7 +6,7 @@ export default function NoteList(props) {
 
   const parsedNotes = props.savedNotes.map(note => {
     if (note.note) {
-    return <NoteListItem content={note.note._immutable} noteId={note.id} key={note.id} title={note.note.title} delete={props.onDelete}text={note.note.text} />
+    return <NoteListItem content={note.note._immutable.currentContent.blockMap} noteId={note.id} key={note.id} title={note.note.title} delete={props.onDelete}text={note.note.text} />
     }
   })
   return (
