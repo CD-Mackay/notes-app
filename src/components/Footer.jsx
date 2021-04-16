fa76df61e96bc12154b9514fb48f66f2ffa7476d
CD-Mackay <connor.mackay@gmail.com>
2021-04-16 11:29:25 -0400

    add elements to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './styles.scss';
 export default function Footer() {
   return (
-    <div className="footer">Heyo!</div>
+    <div className="footer">
+      <p>Connor Mackay</p>
+      <p><a href="https://github.com/CD-Mackay" target="blank">My Github</a></p>
+    </div>
   )
 }
