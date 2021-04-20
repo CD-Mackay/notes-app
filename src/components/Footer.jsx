12c41005a56e1c56fdc0b53316246aed69bb4467
CD-Mackay <connor.mackay@gmail.com>
2021-04-20 14:07:18 -0400

    style anchor link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <p>Connor Mackay</p>
-      <p><a href="https://github.com/CD-Mackay" target="blank">My Github</a></p>
+      <p><a href="https://github.com/CD-Mackay" className="anchor-link" target="blank">My Github</a></p>
     </div>
   )
 }
