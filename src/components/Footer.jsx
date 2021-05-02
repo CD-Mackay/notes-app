addd18865003e1fe1d15ba14814c7416499467b0
CD-Mackay <connor.mackay@gmail.com>
2021-05-02 14:34:58 -0400

    style footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,7 @@ export default function Footer() {
   
   return (
     <div className="footer">
+      <div className="icon-wrapper">
       <h4>Connor Mackay</h4>
       <p><a href="https://github.com/CD-Mackay" className="anchor-link" target="blank">
       <FontAwesomeIcon icon={['fab', 'github-alt']} className="anchor-icon" />
@@ -19,6 +20,7 @@ export default function Footer() {
       <p><a href="mailto: connor.mackay@gmail.com" className="anchor-link" target="blank">
       <FontAwesomeIcon icon={["fa", "envelope"]}  />
       </a></p>
+      </div>
     </div>
   )
 }
