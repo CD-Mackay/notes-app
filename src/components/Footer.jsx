a9a81bebe8e41e748d6ccb85993a1c3bc4215bc1
CD-Mackay <connor.mackay@gmail.com>
2021-05-02 14:32:57 -0400

    style footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,8 +10,14 @@ export default function Footer() {
   return (
     <div className="footer">
       <h4>Connor Mackay</h4>
-      <p><a href="https://github.com/CD-Mackay" className="anchor-link" target="blank">Github
+      <p><a href="https://github.com/CD-Mackay" className="anchor-link" target="blank">
       <FontAwesomeIcon icon={['fab', 'github-alt']} className="anchor-icon" />
+      </a></p>
+      <p><a href="https://www.linkedin.com/in/connor-mackay-800992bb/" className="anchor-link" target="blank">
+      <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+      </a></p>
+      <p><a href="mailto: connor.mackay@gmail.com" className="anchor-link" target="blank">
+      <FontAwesomeIcon icon={["fa", "envelope"]}  />
       </a></p>
     </div>
   )
