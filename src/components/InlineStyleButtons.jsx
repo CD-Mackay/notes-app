41662c48282534ec8b6a11af434726ce9c962bd6
CD-Mackay <connor.mackay@gmail.com>
2021-05-04 16:56:29 -0400

    implement text style modifications

--- src/components/InlineStyleButtons.jsx
+++ src/components/InlineStyleButtons.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export default function InlineStyleButtons(props) {
   return (
-    <button onMouseDown={props.setStyle} key={props.style} textStyle={props.style}>{props.icon}</button>
+    <button onMouseDown={props.setStyle} data-style={props.style} textstyle={props.style}>{props.icon}</button>
   )
 }
