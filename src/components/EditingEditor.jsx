91f0528f1a21fd8308430e7c4cc3d0600684643b
CD-Mackay <connor.mackay@gmail.com>
2021-05-05 12:15:30 -0400

    remove excess code

--- src/components/EditingEditor.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, componentDidMount } from 'react';
-import {Editor, EditorState, convertToRaw, convertFromRaw, createEditorState } from 'draft-js';
-import '../../node_modules/draft-js/dist/Draft.css';
-import './styles.scss';
-
-export 
