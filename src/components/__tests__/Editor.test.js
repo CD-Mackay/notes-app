373b69f620a009959e4c0d1ca2baaa9dba12ef07
Connor Mackay <connor.mackay@gmail.com>
2021-05-27 14:28:34 -0400

    add editor test suite

---
+++ src/components/__tests__/Editor.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { getByTestId, getByText, render, screen } from '@testing-library/react';
+import MyEditor from '../Editor';
+
+describe("MyEditor", () => {
+  it("Renders without Crashing", () => {
+    render(<MyEditor />)
+  });
+});
