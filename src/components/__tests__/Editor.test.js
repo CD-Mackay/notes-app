9dcfecbd8c679fc2215c7db93a69fc09105d89d2
Connor Mackay <connor.mackay@gmail.com>
2021-05-28 16:03:11 -0400

    add test for rendering saved notes

--- src/components/__tests__/Editor.test.js
+++ src/components/__tests__/Editor.test.js
@@ -7,4 +7,40 @@ describe("MyEditor", () => {
    const { getByPlaceholderText } = render(<MyEditor />);
    expect(getByPlaceholderText('note title')).toHaveValue("");
   });
+
+  it("Renders with saved Notes", () => {
+    const selectedNote = {
+      id: 6,
+      title: "Writing notes is fun",
+      category: null,
+      note: {
+      blocks: [
+      {
+      key: "54ig0",
+      data: { },
+      text: "I made a note!",
+      type: "unstyled",
+      depth: 0,
+      entityRanges: [ ],
+      inlineStyleRanges: [ ]
+      },
+      {
+      key: "fu1i",
+      data: { },
+      text: "editing is fun too!",
+      type: "unstyled",
+      depth: 0,
+      entityRanges: [ ],
+      inlineStyleRanges: [ ]
+      }
+      ],
+      entityMap: { }
+      },
+      date_created: "1621275865939",
+      last_modified: "1621276198079"
+      }
+
+      const { getByPlaceholderText } = render(<MyEditor selectedNote={selectedNote} />);
+      expect(getByPlaceholderText('note title')).toHaveValue('Writing notes is fun');
+  })
 });
