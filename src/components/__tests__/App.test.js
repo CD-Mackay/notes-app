d1b7ebb35ce2f804bc7a17f90f507e0d37d7a1f9
Connor Mackay <connor.mackay@gmail.com>
2021-05-31 14:05:20 -0400

    add further app tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,46 +3,20 @@ import { fireEvent, render, getByText, getByPlaceholderText, cleanup } from '@te
 import App from '../../App';
 import NoteList from '../NoteList';
 
-//afterEach(cleanup);
+afterEach(cleanup);
 
 describe("Das Application", () => {
-
-  const notes = [
-    {
-    id: 6,
-    title: "Writing notes is fun",
-    category: null,
-    note: {
-    blocks: [
-    {
-    key: "54ig0",
-    data: { },
-    text: "I made a note!",
-    type: "unstyled",
-    depth: 0,
-    entityRanges: [ ],
-    inlineStyleRanges: [ ]
-    },
-    {
-    key: "fu1i",
-    data: { },
-    text: "editing is fun too!",
-    type: "unstyled",
-    depth: 0,
-    entityRanges: [ ],
-    inlineStyleRanges: [ ]
-    }
-    ],
-    entityMap: { }
-    },
-    date_created: "1621275865939",
-    last_modified: "1621276198079"
-    }
-    ];
     
   it ("Loads without Crashing", () => {
     const { getByText, getByPlaceholderText } = render(<App />);
     expect(getByText("A React App")).toBeInTheDocument;
     expect(getByPlaceholderText("note title")).toBeInTheDocument;
   });
+
+  it("loads the editor and notelist components", () => {
+    const { getByText, getByTestId, getByPlaceholderText } = render(<App />);
+    expect(getByTestId("header")).toBeInTheDocument;
+    expect(getByPlaceholderText("note title")).toHaveValue("");
+  });
+
 });
