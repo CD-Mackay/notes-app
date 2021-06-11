123068744066c81f25de86a3f1032ba25d274a73
Connor Mackay <connor.mackay@gmail.com>
2021-06-11 12:21:21 -0400

    refactor view categories to use buttons

--- src/components/ViewCategories.jsx
+++ src/components/ViewCategories.jsx
@@ -4,22 +4,10 @@ import './Categorystyles.scss';
 export default function ViewCategories(props) {
   return (
     <div className="notesButtons" onChange={props.onSelect}>
-      <div className="view-button-wrapper">
-        <label htmlFor="work" className={props.currentCat === "work" ? "selected-cat" : "cat-button"}>work</label>
-        <input type="radio" id="work" data-testid="workButton" name="category" value="work" ></input>
-      </div>
-      <div className="view-button-wrapper">
-        <label htmlFor="personal" className={props.currentCat === "personal" ? "selected-cat" : "cat-button"}>personal</label>
-        <input type="radio" id="personal" data-testid="personalButton" name="category" value="personal"></input>
-      </div>
-      <div className="view-button-wrapper">
-        <label htmlFor="hobbies" className={props.currentCat === "hobbies" ? "selected-cat" : "cat-button"}>hobbies</label>
-        <input type="radio" id="hobbies" name="category" value="hobbies"></input>
-      </div>
-      <div className="view-button-wrapper">
-        <label htmlFor={props.lastCat} className={props.currentCat === props.lastCat ? "selected-cat" : "cat-button"}>{props.lastCat}</label>
-        <input type="radio" id={props.lastCat} data-testid="allButton" name="category" value={props.lastCat}></input>
-      </div>
+      <button onClick={props.onSelect} value="work">Work</button>
+      <button onClick={props.onSelect} value="personal">Personal</button>
+      <button onClick={props.onSelect} value="hobbies">Hobbies</button>
+      <button onClick={props.onSelect} value="all">All</button>
     </div>
    
   );
