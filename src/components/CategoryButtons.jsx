dd6307f9b7ef41e402895fd46d3cce2a19aa6cbb
Connor Mackay <connor.mackay@gmail.com>
2021-06-23 16:18:10 -0400

    remove catbutton component and refactor categorybuttons to use text input

--- src/components/CategoryButtons.jsx
+++ src/components/CategoryButtons.jsx
@@ -20,25 +20,9 @@ export default function CategoryButtons(props) {
   });
 
   return (
-    <div className="editButtons" onChange={props.onSelectCategory}>
+    <div className="editButtons" >
       <div className="cat-button-wrapper">
-        <label htmlFor="work">work</label>
-        <input type="radio" id="work" name="category" value="work" ></input>
-      </div>
-      <div className="cat-button-wrapper">
-        <label htmlFor="personal">personal</label>
-        <input type="radio" id="personal" name="category" value="personal"></input>
-      </div>
-      <div className="cat-button-wrapper">
-        <label htmlFor="hobbies">hobbies</label>
-        <input type="radio" id="hobbies" name="category" value="hobbies"></input>
-      </div>
-      <div className="cat-button-wrapper">
-        <label htmlFor={props.lastCat}>{props.lastCat}</label>
-        <input type="radio" id={props.lastCat} name="category" value={props.lastCat}></input>
-      </div>
-      <div className="cat-button-wrapper">
-        {/* <input type="text" placeholder="enter new category" value=""></input> */}
+        <input type="text" placeholder="enter new category" onChange={props.onSelectCategory} name="category"></input>
       </div>
     </div>
    
