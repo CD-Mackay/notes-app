be9a36d0cf31e392ad491ee9606b944effb60920
Connor Mackay <connor.mackay@gmail.com>
2021-06-23 16:27:19 -0400

    refactor viewCategories to use dropdown

--- src/components/ViewCategories.jsx
+++ src/components/ViewCategories.jsx
@@ -14,33 +14,16 @@ export default function ViewCategories(props) {
     return note.category;
   });
 
-  useEffect(() => {
     categories = removeDuplicates(categories);
     console.log(categories);
-  });
+    categories = categories.map(cat => <option key={cat} value={cat}>{cat}</option>)
+
 
   return (
     <div className="notesButtons" onChange={props.onSelect}>
-      <button className="modify" data-testid="workButton" onClick={props.onSelect} value="work">
-      <p className="hover-text">&lt;</p>
-        Work
-        <p className="hover-text">/&gt; </p>
-        </button>
-      <button className="modify" data-testid="personalButton" onClick={props.onSelect} value="personal">
-      <p className="hover-text">&lt;</p>
-        Personal
-        <p className="hover-text">/&gt; </p>
-        </button>
-      <button className="modify" onClick={props.onSelect} value="hobbies">
-      <p className="hover-text">&lt;</p>
-        Hobbies
-        <p className="hover-text">/&gt; </p>
-        </button>
-      <button className="modify" data-testid="allButton" onClick={props.onSelect} value="all">
-      <p className="hover-text">&lt;</p>
-        All
-        <p className="hover-text">/&gt; </p>
-        </button>
+      <select name="categories" id="categories">
+      {categories}
+      </select>
     </div>  
    
   );
